b4bd445925534b594c77dcb754c42b98d090f3f6
odedia <PpE7^%&t5yCAAx4TA9EhDbqbZPeybn66>
2019-02-05 08:54:47 +0200

    fix

--- fortune-teller-listener/src/main/java/io/spring/cloud/samples/fortuneteller/fortuneservice/Application.java
+++ fortune-teller-listener/src/main/java/io/spring/cloud/samples/fortuneteller/fortuneservice/Application.java
@@ -26,7 +26,7 @@ import org.springframework.stereotype.Component;
 import lombok.extern.slf4j.Slf4j;
 
 @SpringBootApplication
-@EnableBinding(Source.class)
+@EnableBinding(Sink.class)
 public class Application {
 
     public static void main(String[] args) {
